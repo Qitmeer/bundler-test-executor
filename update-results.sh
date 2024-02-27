222c90c9a76e59a12734d8b6a0ecc059d142a817
frankcrypto <frankcrypto@gmail.com>
2024-02-27 16:46:04 +0800

    fix update

--- update-results.sh
+++ update-results.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 
+# resultrepo=https://github.com/Qitmeer/bundler-test-results.git
 root=`cd \`dirname $0\`;pwd`
+# if [[ -z "$1" || -d "$1" ]] ; then
+#   echo usage: $0 {out-results-clone-folder} [branch]
+#   echo "out folder is created."
+#   echo "(if branch (or GITHUB_REF) is set, the folder is also cloned from $resultrepo)"
+#   exit 1
+# fi
 
 base=$root/$1
+branch=$2
 
 RUNS=$base/runs
 HIST=$base/history
 
 #for local testing. it is set by github action
-#GITHUB_REF=${GITHUB_REF:=`git symbolic-ref  HEAD`}
+# GITHUB_REF=${GITHUB_REF:=`git symbolic-ref  HEAD`}
 
 #starts with "refs/heads/{branchname}"
-
-rm -rf $base
-mkdir -p $base
+# branch=${2:-`echo $GITHUB_REF | perl -pe 's@.*/@@'`}
+# echo $branch
+# rm -rf $base
+# mkdir -p $base
+echo $base
+echo $RUNS
+echo $HIST
+# echo "git clone --depth 1 $resultrepo -b $branch $base"
+#pushing to the same branch in the results repo as this (ux) repo
+# test -n "$branch" && git clone --depth 1 $resultrepo -b $branch $base
 
 ts=`date +%Y%m%d/%H%M%S`
 folder=$RUNS/$ts
+rm -rf $folder
 mkdir -p $folder
 cp build/out/* $folder
 cp -r html/ $folder
 ls -p $folder > $folder/index.txt
 cd $RUNS
+rm -rf latest
 ln -nsf $ts latest
 ls -p > index.txt
 find . > all.txt
-
+rm -rf $HIST
 mkdir -p $HIST
+echo "$root/create-history.sh $RUNS > $HIST/history.json"
 $root/create-history.sh $RUNS > $HIST/history.json
 (echo -n 'testHistory=' ; cat $HIST/history.json) > $HIST/script-history.js
 
